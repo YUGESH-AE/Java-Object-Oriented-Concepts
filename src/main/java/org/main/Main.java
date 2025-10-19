f7833e595d75bbb139caeb443f15eaa328fae72d
YUGESH-AE <yugeshae@gmail.com>
2025-10-19 20:10:31 +0530

    singleton class

---
+++ src/main/java/org/main/Main.java
@@ -0,0 +1,11 @@
+package org.main;
+
+import org.singleton.Singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton s1=Singleton.getInstance();
+        Singleton s2=Singleton.getInstance();
+        System.out.println(s1==s2);
+    }
+}
---
+++ src/main/java/org/singleton/Singleton.java
@@ -0,0 +1,17 @@
+package org.singleton;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    private Singleton(){
+
+    }
+
+    public static Singleton getInstance(){
+        if(instance==null){
+            return instance=new Singleton();
+        }
+        return instance;
+    }
+}
