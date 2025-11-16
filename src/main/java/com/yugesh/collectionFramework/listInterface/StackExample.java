3776501e4125055fc19abaf8527848ca7c7f1e17
YUGESH-AE <yugeshae@gmail.com>
2025-11-16 13:24:56 +0530

    complete list interface with stack and others

---
+++ src/main/java/com/yugesh/collectionFramework/listInterface/StackExample.java
@@ -0,0 +1,29 @@
+package com.yugesh.collectionFramework.listInterface;
+
+import java.util.Stack;
+
+public class StackExample {
+
+    static void main(){
+
+        Stack<String>stack=new Stack<>();
+
+        //push elements
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
+        stack.push("D");
+        System.out.println(stack);
+
+        //peek top of the stack
+        System.out.println(stack.peek());
+
+        //pop removing the top most element
+        System.out.println(stack.pop());
+
+        System.out.println(stack);
+
+        //search the element in the stack
+        System.out.println("Position of A:"+stack.search("A"));
+    }
+}
