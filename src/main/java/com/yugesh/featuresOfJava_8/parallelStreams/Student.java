37c9952ac1e2caf44a3b7c3c8c20d54f7050805d
YUGESH-AE <yugeshae@gmail.com>
2026-01-01 18:13:54 +0530

    stream parallel

--- src/main/java/com/yugesh/featuresOfJava_8/parallelStreams/Student.java
+++ src/main/java/com/yugesh/featuresOfJava_8/parallelStreams/Student.java
@@ -1,8 +1,6 @@
 package com.yugesh.featuresOfJava_8.parallelStreams;
 
-import com.github.javafaker.IdNumber;
-import com.github.javafaker.Number;
-import com.github.javafaker.PhoneNumber;
+
 import lombok.*;
 
 @ToString
